87dbfc2182fd1868c845a1d2ac88e0f599967164
Ebenezer  Semere <ebenezersemere@Ebenezers-MacBook-Air.local>
2023-02-04 21:21:45 -0800

    Setting up LMM

--- code/nlp/lm/LambdaLMModel.java
+++ code/nlp/lm/LambdaLMModel.java
@@ -2,32 +2,104 @@ package nlp.lm;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class LambdaLMModel implements LMModel{
     private double lambda;
     private ArrayList<String> tokens;
 
+    /**
+     * Trains a language model on the given file and lambda value.
+     * We want the contents of the file to be stored in a main list, where sentences are separated by <s> and </s>.
+     * We also want to replace the first occurrence of each word with <unk>.
+     */
+
     public LambdaLMModel(String filename, double lambda){
         this.lambda = lambda;
 
+        // process file
         File file = new File(filename);
-        ArrayList<String> lines = new ArrayList<String>();
+        ArrayList<String> main = new ArrayList<String>();
+
+        HashMap<String, Integer> wordCounts = new HashMap<String, Integer>();
+        wordCounts.put("<unk>", 0);
+        wordCounts.put("<s>", 0);
 
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
-                String[] line = ("<s> " + scanner.nextLine() + " </s>").split(" ");
-                lines.addAll(Arrays.asList(line));
+                String[] line = scanner.nextLine().split("\\s");
+
+                wordCounts.put("<s>", wordCounts.get("<s>") + 1);
+                main.add("<s>");
+
+                for (String word: line) {
+                    if (!wordCounts.containsKey(word)) {
+                        wordCounts.put("<unk>", wordCounts.get("<unk>") + 1);
+                        wordCounts.put(word, 0);
+                        main.add("<unk>");
+                    } else {
+                        wordCounts.put(word, wordCounts.get(word) + 1);
+                        main.add(word);
+                    }
+                }
+
+                main.add("</s>");
+
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + file);
         }
 
-        this.tokens = lines;
+        // create bigram
+        HashMap<String, HashMap<String, Double>> bigram = new HashMap<String, HashMap<String, Double>>();
+
+        for (int i = 0; i < main.size() - 1; i++) {
+            String curToken = main.get(i);
+            String nextToken = main.get(i + 1);
+
+            if (curToken.equals("</s>")){
+                continue;
+            }
+
+            if (!bigram.containsKey(curToken))
+                bigram.put(curToken, new HashMap<String, Double>());
+
+            if (!bigram.get(curToken).containsKey(nextToken))
+                bigram.get(curToken).put(nextToken, 0.0);
+
+            double numerator = bigram.get(curToken).get(nextToken) + 1.0;
+//            double denominator = wordCounts.get(curToken);
+
+//            System.out.println(curToken + " " + nextToken + " " + numerator + " " + denominator);
+
+            bigram.get(curToken).put(nextToken, numerator);
+        }
+
+
+        // normalize bigram probabilities over all words with lambda smoothing
+        for (String outerKey: bigram.keySet()){
+            double denominator = wordCounts.get(outerKey);
+            for (String innerKey: bigram.get(outerKey).keySet()){
+                if (innerKey.equals("</s>"))
+                    continue;
+
+                double numerator = bigram.get(outerKey).get(innerKey);
+//                System.out.println(innerKey);
+//                System.out.println(wordCounts.get(innerKey));
+                bigram.get(outerKey).put(innerKey, (numerator + lambda) / (denominator + (lambda * wordCounts.get(innerKey))));
+            }
+        }
+
+//
+//        System.out.println(main);
+//        System.out.println(wordCounts);
+        System.out.println(bigram);
     }
 
+    /**
+     * We utilize the hashtable to find the bigram value of each pair of words in our model. We simply return the
+     * product of the probabilities at every bigram.
+     */
     public double logProb(ArrayList<String> sentWords){
         return 0.0;
     }
@@ -36,18 +108,29 @@ public class LambdaLMModel implements LMModel{
         return 0.0;
     }
 
+    /**
+     * We utilize a hashtable of hashtables, where the keys of the master hashtable are all the words of the corpus,
+     * and the values are all hashtables, whose keys are all words that follow the master key, and whose values are
+     * the bigram value of the two words.
+     *
+     * For example, {hello: {sunshine: .01}, {neighbor: .1}, {world: .89}}
+     *
+     * So, given a first and second word, we access the master hashtable at the first word, and return the
+     * value at the key of the second word.
+     *
+     * getBigramProb(hello, world) -> return p(world|hello) -> return words[hello][world]
+     */
     public double getBigramProb(String first, String second){
         return 0.0;
     }
 
     public static void main(String[] args) {
 
-        String filename = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences";
-        double lambda = 0.5;
+//        String filename = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences";
+        String filename = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/abc.txt";
+        double lambda = 1.0;
 
         LambdaLMModel model = new LambdaLMModel(filename, lambda);
-        System.out.println(model.tokens);
-
     }
 
 }
