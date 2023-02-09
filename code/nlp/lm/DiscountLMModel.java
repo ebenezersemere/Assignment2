8e903975b507acd2839b44f4d2ec443f15abed2d
Ebenezer  Semere <ebenezersemere@Ebenezers-MacBook-Air.local>
2023-02-08 23:06:53 -0800

    Testing

--- code/nlp/lm/DiscountLMModel.java
+++ code/nlp/lm/DiscountLMModel.java
@@ -1,5 +1,8 @@
 package nlp.lm;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A class for bigram language models with discount smoothing
  */
@@ -47,12 +50,56 @@ public class DiscountLMModel extends LMBase{
 
 
     public static void main(String[] args) {
-        String filenameMain = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences";
-        String filenameABC = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/abc.txt";
+//        String filenameMain = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences";
+//        String filenameABC = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/abc.txt";
+//
+//        double discount = 0.5;
+//
+//        DiscountLMModel model = new DiscountLMModel(filenameABC, discount);
 
-        double discount = 0.5;
+        String filenameTrain = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences_train";
+        String filenameDev = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences_development";
+        String filenameTest = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences_testing";
 
-        DiscountLMModel model = new DiscountLMModel(filenameABC, discount);
+//        double discount = .1;
+
+        List<Double> discount = Arrays.asList(0.99, 0.9, 0.75, 0.5, 0.25, 0.1);
+
+        for (double disc : discount){
+            LambdaLMModel model = new LambdaLMModel(filenameTrain, disc);
+            double perplexity = model.getPerplexity(filenameTest);
+            System.out.println(perplexity);
+            System.out.println(disc);
+        }
+
+//        // Dev
+//        2210.1003431764807
+//        0.99
+//        2106.3371327748073
+//        0.9
+//        1924.609132693029
+//        0.75
+//        1588.2300828933421
+//        0.5
+//        1176.5117951541981
+//        0.25
+//        837.5045562172319
+//        0.1
+
+//        // Test
+//        2145.079988806118
+//        0.99
+//        2043.8582581124003
+//        0.9
+//        1866.658917449232
+//        0.75
+//        1538.9459009003788
+//        0.5
+//        1138.4251166815525
+//        0.25
+//        809.2941685685007
+//        0.1
+
     }
 }
 
--- code/nlp/lm/LambdaLMModel.java
+++ code/nlp/lm/LambdaLMModel.java
@@ -39,10 +39,56 @@ public class LambdaLMModel extends LMBase{
 //        String filenameMain = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences";
 //        String filenameABC = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/abc.txt";
 
-        String filenameTrain = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentencesTrain";
+        String filenameTrain = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences_train";
+        String filenameDev = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences_development";
+        String filenameTest = "/Users/ebenezersemere/Workspace/Natural Language Processing/Assignment2/data/sentences_testing";
 
-        double lambda = 1.0;
 
-        LambdaLMModel model = new LambdaLMModel(filenameTrain, lambda);
+        double lambda = .1;
+
+        for (int i = 0; i < 8; i++){
+            LambdaLMModel model = new LambdaLMModel(filenameTrain, lambda);
+            double perplexity = model.getPerplexity(filenameTest);
+            System.out.println(perplexity);
+            System.out.println(lambda);
+            lambda /= 10;
+        }
+
+//        // Development
+//        837.5045562172319
+//        0.1
+//        481.5731773218983
+//        0.01
+//        425.7744190175921
+//        0.001
+//        520.4360899049848
+//        1.0E-4
+//        745.7024733316655
+//        1.0E-5
+//        1120.0851147061799
+//        1.0000000000000002E-6
+//        1695.2085289590827
+//        1.0000000000000002E-7
+//        2567.7680339278627
+//        1.0000000000000002E-8
+
+//        // Testing
+//        809.2941685685007
+//        0.1
+//        464.8076416605193
+//        0.01
+//        410.7734991977494
+//        0.001
+//        500.77403478931325
+//        1.0E-4
+//        713.8558210497071
+//        1.0E-5
+//        1065.475759362419
+//        1.0000000000000002E-6
+//        1601.989195913179
+//        1.0000000000000002E-7
+//        2410.5953397969947
+//        1.0000000000000002E-8
+
     }
 }
